7bf280d6cd6d461cbb61113ce7908b2b3684b364
vorvor <vorosborisz@gmail.com>
2014-07-24 15:35:37 +0200

    translation, theming

---
+++ inc/activitystatement.php
@@ -0,0 +1,49 @@
+<h1>Activity Statement</h1>
+
+<table><tr><th id="title" colspan="2">Megnevezés</th><th id="amount" colspan="3">in Eur</th></tr>
+<tr><td><strong>A.</strong></td><td><strong>Capital opening balance</td><td></td><td></td><td><strong>1 488 648</strong></td></tr>
+<tr><td><strong>B.</strong></td><td><strong><strong>Income</strong></td><td></td><td></td><td></td></tr>
+<tr><td></td><td>1. Funds received for public benefit activities</td><td></td><td></td><td></td></tr>
+<tr><td></td><td>2. Grant support</td><td></td><td>2 394 803</td><td></td></tr>
+<tr><td></td><td>3. Income of own activities</td><td></td><td>2 363</td><td></td></tr>
+<tr><td></td><td>4. Income of own enterprise</td><td></td><td>17 607</td><td></td></tr>
+<tr><td></td><td>5. Fund received from business entities</td><td></td><td>68 400</td><td></td></tr>
+<tr><td></td><td>6. Income from 1% income tax</td><td></td><td>341</td><td></td></tr>
+<tr><td></td><td>7. Bank income</td><td></td><td>95 994</td><td></td></tr>
+<tr><td></td><td><strong>Összes bevétel</strong></td><td></td><td></td><td><strong>770757</strong></td></tr>
+
+<tr><td><strong>C.</strong></td><td><strong>Expenses</strong></td><td></td><td></td><td></td></tr>
+<tr><td></td><td>6. Personnel</td><td></td><td>113 357</td><td></td></tr>
+<tr class="emp"><td></td><td>7. Programme costs</td><td></td><td>121 064</td><td></td></tr>
+<tr><td></td><td><strong>PROGRAMS</strong></td><td></td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Future Team</td><td>2 209</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Alternatives to nuclear energy</td><td>9 207</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Green Belt Program</td><td>8 149</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Civil Partner Program</td><td>11 787</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">NGO Fund of the EEA/Norwegian Grants</td><td>65 325</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Tree of the Year</td><td>1 078</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Swiss-Hungarian NGO Block Grant and Scholarship Fund</td><td>23 310</td><td></td><td></td></tr>
+
+<tr class="emp"><td></td><td>8. Grants</td><td></td><td>2 227 108</td><td></td></tr>
+<tr><td></td><td class="lpad">Grants given in Hungary *</td><td>1 844 183</td><td></td><td></td></tr>
+<tr><td></td><td class="lpad">Forwarded support **</td><td>382 925</td><td></td><td></td></tr>
+
+<tr><td></td><td>9. Fixed assets and depreciation</td><td></td><td>12 021</td><td></td></tr>
+<tr><td></td><td>10. Bank costs</td><td></td><td>23 534</td><td></td></tr>
+<tr><td></td><td>11. Other expenses</td><td></td><td>2 343</td><td></td></tr>
+<tr><td></td><td><strong>Total Expenses</strong></td><td></td><td></td><td><strong>2 499 428</strong></td></tr>
+
+<tr><td><strong>D.</strong></td><td><strong>Result of current year</strong></td><td></td><td><strong>80 080</strong></td><td></td></tr>
+<tr><td></td><td></td><td></td><td></td><td></td></tr>
+<tr><td><strong>E.</strong></td><td><strong>Capital closing balance</strong> </td><td></td><td></td><td><strong>1 568 728</strong></td></tr>
+<tr class="emp"><td></td><td>Start up fund</td><td></td><td></td><td>669</td></tr>
+<tr class="emp"><td></td><td>Capital total</td><td></td><td></td><td>1 569 398</td></tr>
+
+<tr class="emp"><td></td><td>Own capital</td><td></td><td></td><td>1 569 398</td></tr>
+</table>
+<div class="table-footer"><p>** Without grants of the EEA/Norwegian NGO Fund, the monies of which do not go through HEPF's account.</p></div>
+<div class="table-footer"><p>** * Management costs forwarded to the consortium partners of the Swiss and Norwegian NGO Funds.</p></div>
+
+<div class="table-footer"><p><a href="./attachments/audit okotars 2013.pdf">Download Auditor’s report</a></p></div>
+<div class="table-footer"><p><a href="./attachments/Kozhasznusagi_jelentes_2013.pdf">Public benefit report</a></p></div>
+
---
+++ inc/balancesheet.php
@@ -0,0 +1,38 @@
+<h1>Balance sheet of simplified financial report</h1>
+
+<table><tr><th id=title></th><th id=prevyear>Previous year (EUR)</th><th id=actyear>Current year (EUR)</th></tr>
+<tr class="emp"><td>ASSETS</td><td></td><td></td></tr>
+<tr class="emp"><td>A. Fixed assets</td><td>496 934,40</td><td>518 594,38</td></tr>
+<tr><td class="lpad">I. Intangible assets</td><td>261,04</td><td>0,00</td></tr>
+<tr><td class="lpad">II. Tangible assets</td><td>162 315,93</td><td>171 101,07</td></tr>
+<tr><td class="lpad">III. Long term investments</td><td>334 357,43</td><td>347 493,31</td></tr>
+<tr class="emp"><td>B. Current assets</td><td>987 175,37</td><td>1 874 779,12</td></tr>
+<tr><td class="lpad">I. Inventory</td><td>2 851,41</td><td>2 851,41</td></tr>
+<tr><td class="lpad">II. Accounts receivable</td><td>84 313,92</td><td>127 503,35</td></tr>
+<tr><td class="lpad">III. Short term investment (recurities)</td><td>621 807,23</td><td>522 841,37</td></tr>
+<tr><td class="lpad">IV. Cash and cash equivalents</td><td>278 202,81</td><td>1 221 583,00</td></tr>
+<tr><td>C. Prepaid expenses</td><td>31 720,21</td><td>1 556 777,11</td></tr>
+<tr class="emp"><td>Total assets</td><td>1 515 829,99</td><td>3 950 150,60</td></tr>
+<tr><td>SOURCES</td><td></td><td></td></tr>
+<tr class="emp"><td>D. Own capital*</td><td>1 489 317,27</td><td>1 569 397,59</td></tr>
+<tr><td class="lpad">I. Start up fund balance</td><td>669,34</td><td>669,34</td></tr>
+<tr><td class="lpad">II. Change of fund balance</td><td>1 566 613,12</td><td>1 488 647,93</td></tr>
+<tr><td class="lpad">III.Tied up reserve</td><td>0,00</td><td>0,00</td></tr>
+<tr><td class="lpad">IV. Valuation reserve</td><td>0,00</td><td>0,00</td></tr>
+<tr><td class="lpad">"V. Result of current year of basic activity /public benefit activity"</td><td>-90 759,71</td><td>68 594,38</td></tr>
+<tr><td class="lpad">"VI. Result of current year from enterprise"</td><td>12 794,51</td><td>11 485,94</td></tr>
+<tr><td>E. Marked reserve</td><td>0,00</td><td>0,00</td></tr>
+<tr class="emp"><td>F. Liabilities</td><td>24 461,18</td><td>1 552 674,03</td></tr>
+<tr><td class="lpad">I. Subordinated liabilities</td><td>0,00</td><td>0,00</td></tr>
+<tr><td class="lpad">II. Long term liabilities</td><td>0,00</td><td>0,00</td></tr>
+<tr><td class="lpad">III. Short term liabilities</td><td>24 461,18</td><td>1 552 707,50</td></tr>
+<tr><td class="lpad">G. Accrued expenses</td><td>2 051,54</td><td>828 078,98</td></tr>
+<tr class="emp"><td>Total net worth and liabilities</td><td>1 515 829,99</td><td>3 950 150,60</td></tr>
+</table>
+
+
+
+<div class=table-footer><p>* Grants contracted in 2013, but not yet paid out (fully or partially).</div>
+
+
+
